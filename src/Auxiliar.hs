543b5f35a0b14be9c2eac9b6f242865e37b3a7ef
Raphael Agra <raphael.agra@ccc.ufcg.edu.br>
2021-04-23 20:32:40 -0300

    Dei continuidade em algumas funcionalidades.

--- src/Auxiliar.hs
+++ src/Auxiliar.hs
@@ -1,14 +1,14 @@
+module Auxiliar where
+
 import qualified Usuario as Usuario
+import qualified Banco as Banco
 import Data.List
 import System.IO
 import Data.Map as Map 
-import Data.Listc
+import Data.List
 import Data.List.Split (splitOn)
 
 
-
-
--- INCOMPLETO E NAO TESTADO
 {-
 raphael,111,Nubank 120,Itau 300, Bradesco 50
 nicolas,222,Inter 200                            -- LISTA INICIAL NO dados/usuarios.txt
@@ -17,7 +17,7 @@ vh,444,Santander 400
 -}
 {-
 [["raphael","111","Nubank 120"],
-["nicolas","222","Inter 200"],      -- APOS EXECUTAR A LINHA 34
+["nicolas","222","Inter 200"],      -- APOS EXECUTAR A LINHA 32
 ["henrique","333","Pan 300"],
 ["vh","444","Santander 400"]]
 -}
@@ -33,15 +33,44 @@ getListaDeUsuarios = do
     arquivo <- readFile "dados/usuarios.txt"
     let lista = ((Data.List.map ( splitOn ",") (lines arquivo)))
     let lista_usuarios = ((Data.List.map constroiUsuario lista))
-    return lista_usuarios
+    return lista_usuarios   
 
+-- String inicial ["raphael","senhaDeRaphael","Nubank 120", "Inter 200"]
 constroiUsuario :: [String] -> Usuario.Usuario
 constroiUsuario lista = Usuario.Usuario {
     Usuario.login = lista !! 0,
     Usuario.senha = lista !! 1,
-    Usuario.bancos = drop 2 lista
+    Usuario.bancos = (getListaDeBancos (Data.List.drop 2 lista))
+}
+ 
+ {-
+ ["Nubank 120", "Inter SA 200"]   EXEMPLO DE ENTRADA
+
+
+[Banco {nome = "Nubank", saldo = 120.0},    SAIDA
+ Banco {nome = "Inter SA", saldo = 200.0}]
+-}
+getListaDeBancos :: [String] -> [Banco.Banco]
+getListaDeBancos arquivo = (Data.List.map constroiBanco (Data.List.map ( splitOn " ") (arquivo)))
+
+constroiBanco :: [String] -> Banco.Banco
+constroiBanco lista = Banco.Banco {
+    Banco.nome = Data.List.intercalate " " (Data.List.take ((length lista) - 1) lista),
+    Banco.saldo = (read (last lista))
 }
 
+
+
+
+
+
+
+
+
+
+
+
+{- COMENTEI PQ NAO TAVA FUNCIONANDO
 
 
 --Retorna a lista de um usuário cadastrado no sistema
@@ -49,19 +78,17 @@ constroiUsuario lista = Usuario.Usuario {
 getListaLogin :: [[String]] -> String -> [[String]]
 getListaLogin lista login = Data.List.filter (login `elem`) lista
 
-existeLogin :: [String] -> Bool
+existeLogin :: [String] -> IO(Bool)
 existeLogin login = do
+    arquivo <- readFile "dados/logins.txt"
     let lista_usuarios = ((Data.List.map ( splitOn ",") (lines arquivo)))
     let lista_login = getListaLogin lista_usuarios login
-    if null lista_login
-        then True
-        else False
+    if Data.List.null lista_login
+        then return True
+        else return False
 
 
-
-
-exibirUsuario :: String -> [String]
-exibirUsuario login = getListaDeUsuarios !! 0
+exibirUsuario :: String -> IO(Usuario.Usuario)
+exibirUsuario login = getListaLogin login !! 0
     
-    
-    
+-}    
