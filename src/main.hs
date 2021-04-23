fd5b3f40239e5c65642c69a2adeb5de03d4132c1
Raphael Agra <raphael.agra@ccc.ufcg.edu.br>
2021-04-23 20:31:00 -0300

    Sistema de login no menu.

--- src/main.hs
+++ src/main.hs
@@ -1,8 +1,13 @@
+import Auxiliar as Auxiliar
+import Usuario as Usuario
+import Banco as Banco
 import System.IO
 import System.Directory
 import Data.Char
 import Data.List
 import Data.List.Split (splitOn)
+
+
 
 
 main :: IO ()
@@ -14,40 +19,54 @@ main = do
 menuLogin :: IO()
 menuLogin = do
 	putStr ("Selecione uma das opções:\n\n" ++ 
-		"  (E)xibir dados \n" ++ 
+		"  (L) Fazer login \n" ++ 
 		"  (C) Cadastrar usuário\n" ++ 
 		"  (S) Sair\n\n" ++
 		"Opção> ")
 	
 	opcao <- getLine 
 
-	if (toUpperCase opcao) == "E"
+	if (toUpperCase opcao) == "L"
 		then do
 			
 			putStr "\nLogin: "
-	
 			login <- getLine
+			putStr "Senha: "
+			senha <- getLine
 
 			arquivo <- readFile "dados/usuarios.txt"
 			
 			if existeLogin login arquivo
-				then print(exibirUsuario login arquivo)
-				else putStrLn "Usuario não cadastrado"
+				then 
+					if verificaSenha login senha arquivo
+						then fazerLogin login 
+						else putStrLn "Senha incorreta. Tente novamente\n"
+				else putStrLn "Usuario não existe. Tente novamente com outro login\n"
 			
+			menuLogin
 
 			
 	else if (toUpperCase opcao) == "C"
 		then do
 			
+			putStrLn "\nCrie um login e uma senha\nA senha deve ter 6 ou mais caracteres e conter 1 caracter especial('*', '!', '@', '/', '#')"
+			
 			putStr "\nLogin: "
 			login <- getLine 
-
-			putStrLn "\nA senha deve ter 6 ou mais caracteres e conter 1 caracter especial('*', '!', '@', '/', '#')"
 			putStr "Senha: "
 			senha <- getLine
 
-			cadastraUsuario login senha
-
+			arquivo <- readFile "dados/usuarios.txt"
+			
+			if existeLogin login arquivo
+				then 
+					do
+					putStrLn "Login ja cadastrado. Tente novamente com outro login\n"
+					menuLogin
+				else cadastraUsuario login senha
+			
+			menuLogin
+			
 			
 	else if (toUpperCase opcao) == "S"
 		then do
@@ -55,21 +74,18 @@ menuLogin = do
 
 	else 
 		do
-			putStrLn "\nOpcao invalida!\n"
+			putStrLn "Opcao invalida!\n"
 			menuLogin
 
 
+fazerLogin :: String -> IO()
+fazerLogin login = do
+	putStrLn "NAO IMPLEMENTADO\n"
 
 
-
-
--- Recebe uma string e tranforma tudas as letras minusculas em maiusculas
-toUpperCase :: String -> String
-toUpperCase palavra = [toUpper x | x <- palavra]
 
 ----------------------------------------------------------------
 --CadastroUsuario
-
 
 -- Função que irá cadastrar o usuário num arquivo .txt, mantendo um registro dos usuarios cadastrados
 cadastraUsuario :: String -> String -> IO ()
@@ -77,15 +93,13 @@ cadastraUsuario login senha =
     if validaCadastro login senha
         then
             do
-            file <- openFile "dados/usuarios.txt" ReadMode
-            content <- hGetContents file
-            writeToFile login senha content
-            hClose file
-
-            removeFile "dados/usuarios.txt"    
-            renameFile "dados/usuariosCorreto.txt" "dados/usuarios.txt" 
-            putStrLn "Cadastro realizado com sucesso"
-        else putStrLn "Cadastro não realizado"
+            appendFile "dados/usuarios.txt" ((toUpperCase login) ++ "," ++ senha ++ "\n")
+            putStrLn "Cadastro realizado com sucesso\n"
+			
+        else 
+			do
+			putStrLn "Nao foi possivel realizar o cadastro. Tente novamente\n"
+			
 
 -- -- Função que vai ser responsável por validar o cadastro do Usuário
 -- -- Login não pode ser vazio
@@ -99,12 +113,17 @@ validaCadastro login senha =
         then False
         else not (null (filter (`elem` ['*', '!', '@', '/', '#']) senha))
 
---Função que vai ser responsável por escrever os usuários em um arquivo
-writeToFile :: String -> String -> String -> IO ()      
-writeToFile login senha conteudo = do
-    if (null conteudo)
-        then writeFile "dados/usuariosCorreto.txt" ("Login: " ++ login ++ "\n" ++ "Senha: " ++ senha)
-        else writeFile "dados/usuariosCorreto.txt" (conteudo ++ "\n" ++ "Login: " ++ login ++ "\n" ++ "Senha: " ++ senha)
+
+
+
+
+carregaUsuarios :: IO([Usuario.Usuario])
+carregaUsuarios = Auxiliar.getListaDeUsuarios 
+
+
+
+
+
 
 -------------------------------------------------------------------------------------
 
@@ -121,13 +140,28 @@ getListaLogin lista login = Data.List.filter (login `elem`) lista
 existeLogin :: String -> String -> Bool
 existeLogin login arquivo = do
     let lista_usuarios = ((Data.List.map ( splitOn ",") (lines arquivo)))
-    let lista_login = getListaLogin lista_usuarios login
+    let lista_login = getListaLogin lista_usuarios (toUpperCase login)
     if null lista_login
         then False
         else True
+
+-- verifica se a senha de um determinado login esta correta ou nao
+verificaSenha :: String -> String -> String -> Bool 
+verificaSenha login senha arquivo = do
+	let lista_usuarios = ((Data.List.map ( splitOn ",") (lines arquivo)))
+	let lista_login = getListaLogin lista_usuarios (toUpperCase login)
+	if (lista_login !! 0) !! 1 == senha
+		then True 
+		else False
 
 
 exibirUsuario :: String -> String -> [String]
 exibirUsuario login arquivo = do
 	let lista_login = getListaLogin ((Data.List.map ( splitOn ",") (lines arquivo))) login
 	head lista_login
+
+
+
+-- Recebe uma string e tranforma tudas as letras minusculas em maiusculas
+toUpperCase :: String -> String
+toUpperCase palavra = [toUpper x | x <- palavra]
