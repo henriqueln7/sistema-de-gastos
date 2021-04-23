58d228ec1e186fe0f09c7da7d5ff66f3de7a7806
Raphael Agra <raphael.agra@ccc.ufcg.edu.br>
2021-04-23 20:31:42 -0300

    Funcionalidade getSaldoTotal implementada.

--- src/Usuario.hs
+++ src/Usuario.hs
@@ -1,7 +1,20 @@
 module Usuario where
+import qualified Banco as Banco
 
 
-data Usuario = Usuario {login :: String, senha :: String, bancos :: [Banco]} deriving (Eq, Show, Read)
+data Usuario = Usuario {login :: String, senha :: String, bancos :: [Banco.Banco]} deriving (Eq, Show, Read)
 
 
-        
+getSaldoTotal :: Usuario -> Float 
+getSaldoTotal user = sum (getListaDeSaldos (bancos user))
+
+getListaDeSaldos :: [Banco.Banco] -> [Float] 
+getListaDeSaldos [] = []
+getListaDeSaldos (h:t) = Banco.saldo h : getListaDeSaldos t
+
+
+
+
+
+
+
