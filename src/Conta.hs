248a566e264c0c59fb7577e8a3e0eae99d287354
DellAero <Henrique Lopes>
2021-04-26 15:40:36 -0300

    Adiciona funçao para registrar receita

--- src/Conta.hs
+++ src/Conta.hs
@@ -1,6 +1,9 @@
-data TipoConta = Corrente | Poupanca | Carteira deriving (Show)
+data TipoConta = Corrente | Poupanca | Carteira deriving (Show, Eq)
 
 data Conta = Conta {contaNome :: String, contaCodigo :: String, saldoConta :: Double, tipoConta :: TipoConta, descricao :: String} deriving (Show)
+
+instance Eq Conta where
+    c1 == c2 = (==) (contaCodigo c1) (contaCodigo c2)
 
 data CartaoCredito = CartaoCredito {cartaoNome :: String, cartaoLimite :: Float, dataVencimento :: String, cvv :: String} deriving (Eq, Show, Read)
 
@@ -11,22 +14,13 @@ type InformacaoConta = (String, Double)
 verificarSaldo :: Usuario -> [InformacaoConta]
 verificarSaldo usuario = map (\conta -> (contaNome conta, saldoConta conta)) (contas usuario)
 
-data TipoTransacao = Receita | Despesa | Transferencia
-
--- ContaTransacao é em qual conta a operação está sendo realizada
--- data Transacao = Transacao {valorTransacao :: Double, dataTransacao :: String, contaTransacao :: Conta, tipoTransacao :: TipoTransacao}
-
 data CategoriaReceita = Emprestimo | Investimento | Salario | OutrasReceitas
 data TransacaoReceita = TransacaoReceita {valorTransacaoReceita :: Double, categoriaReceita :: CategoriaReceita, dataReceita :: String, contaDestino :: Conta}
 
+-- Aqui ainda tá duplicando a conta dentro de usuário :(
 registrarReceita :: TransacaoReceita -> Usuario ->  Usuario
-registrarReceita TransacaoReceita {valorTransacaoReceita = valor, contaDestino = contaDestino} usuario = usuario {contas = contas usuario ++ [contaDestino {saldoConta = saldoConta contaDestino + valor}]}
+registrarReceita TransacaoReceita {valorTransacaoReceita = valor, contaDestino = contaDestino} usuario = usuario {contas = alterarValorConta (contaDestino {saldoConta = saldoConta contaDestino + valor}) (contas usuario) }
 
--- registrarDespesa :: Transacao -> Conta
--- registrarDespesa transacao = let conta = contaTransacao transacao in conta {saldoConta = saldoConta conta - valorTransacao transacao}
-
--- registrarTransferencia :: Transacao -> Conta -> Conta -> (Conta, Conta)
--- registrarTransferencia transacao c1 c2 =
---   ( c1 {saldoConta = saldoConta c1 - valorTransacao transacao},
---     c2 {saldoConta = saldoConta c2 + valorTransacao transacao}
---   )
+-- Pega o valor de uma determinada conta dentro da lista de conta, altera e retorna uma nova lista.
+alterarValorConta :: Conta -> [Conta] -> [Conta]
+alterarValorConta contaParaAlterar = map (\c -> if c == contaParaAlterar then contaParaAlterar else c)
