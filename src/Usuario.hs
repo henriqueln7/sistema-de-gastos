d47597d5518c3b139ac936f7a592c628cf03c046
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2021-04-30 09:56:16 -0300

    Adiciona tratamento de erro para o caso em que usuario nao possui saldo na conta para realizar a transferencia

--- src/Usuario.hs
+++ src/Usuario.hs
@@ -2,30 +2,27 @@ module Usuario where
 
 import Conta
 
-
 data Usuario = Usuario {login :: String, senha :: String, contas :: [Conta]} deriving (Eq, Show, Read)
 
-
-getSaldoTotal :: Usuario -> Double 
+getSaldoTotal :: Usuario -> Double
 getSaldoTotal user = sum (map saldoConta (contas user))
 
+data Transferencia = Transferencia {usuario :: Usuario, contaOrigem :: Conta, contaDestino :: Conta, valor :: Double} deriving (Show)
 
-data Transferencia = Transferencia {usuario :: Usuario, contaOrigem :: Conta, contaDestino :: Conta, valor :: Double } deriving (Show)
-
-realizaTransacao :: Transferencia -> Usuario
-realizaTransacao Transferencia {usuario=usuario,contaOrigem=contaOrigem,contaDestino=contaDestino,valor=valor} =
-                                                                let novaContaOrigem = contaOrigem {saldoConta = saldoConta contaOrigem - valor}
-                                                                    novaContaDestino = contaDestino {saldoConta = saldoConta contaDestino + valor}
-                                                            in
-                                                                usuario {contas = alterarValoresContas (novaContaOrigem, novaContaDestino) (contas usuario)}
+realizaTransacao :: Transferencia -> Maybe Usuario
+realizaTransacao
+  Transferencia
+    { usuario = usuario,
+      contaOrigem = contaOrigem,
+      contaDestino = contaDestino,
+      valor = valor
+    } =
+    let novaContaOrigem = contaOrigem {saldoConta = saldoConta contaOrigem - valor}
+        novaContaDestino = contaDestino {saldoConta = saldoConta contaDestino + valor}
+     in if saldoConta novaContaOrigem < 0
+          then Nothing
+          else Just usuario {contas = alterarValoresContas (novaContaOrigem, novaContaDestino) (contas usuario)}
 
 -- Pega o valor de uma determinada conta dentro da lista de conta, altera e retorna uma nova lista.
 alterarValoresContas :: (Conta, Conta) -> [Conta] -> [Conta]
-alterarValoresContas (contaOrigem, contaDestino) contas = map (\conta -> if conta == contaOrigem  then contaOrigem else if conta == contaDestino then contaDestino else conta) contas
-
-
-
-
-
-
-
+alterarValoresContas (contaOrigem, contaDestino) contas = map (\conta -> if conta == contaOrigem then contaOrigem else if conta == contaDestino then contaDestino else conta) contas
