a38ef1f0c123a3b6e398d821582b72f9abb2a6aa
nicolasmnl <nicolas.leite@ccc.ufcg.edu>
2021-05-19 22:14:22 -0300

    Adicionando o usuario na base de dados quase completo(verificar se usuario já está cadastrado precisa ser feito)

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -1,5 +1,7 @@
 :- use_module(library(readutil)).
 
+%Preciso fazer ainda
+%	*Checagem se já existe Login na base na hora de cadastrar um usuario
 main :- menuInicial, halt.
 
 banner :-write("
@@ -11,7 +13,7 @@ banner :-write("
 ░╚═════╝░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝  ╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░╚════╝░╚═════╝░
 ").
 
-menuInicial :-tty_clear,
+menuInicial :-nl,
 	banner,
 	write("
 Selecione uma das opções abaixo: 
@@ -52,20 +54,20 @@ A senha deve ter 6 ou mais caracteres e conter 1 caracter especial('*', '!', '@'
 	write("\nLogin: "), 
 	lerEntrada(Login), 
 	write("Senha: "), 
-	lerEntrada(Senha), 
-	write("Conta: "),
-	lerEntrada(Conta),
-	usuario(Login, Senha,Conta, User),
-	open('dados/usuarios.txt',append, Str),
-	writeln(Str, User),
-	close(Str),nl,halt.
+	lerEntrada(Senha),
+	string_to_atom(Senha, AtomoSenha),
+	atom_chars(AtomoSenha,L),
+	(validaSenha(L) -> usuario(Login, Senha, User),
+					   open('dados/usuarios.txt',append, Str),
+					   writeln(Str, User),
+					   close(Str), nl,write("Usuario Cadastrado"),nl, menuInicial; 
+		nl, write("Senha Invalida, por favor tente novamente"),nl, menu("C")).
+	
 
-
-
-listaUsuarios(R) :- R is [].
-
-
-
+%Recebo a senha como uma lista já
+validaSenha(Senha) :- checaCaracterEspecial(Senha), checaTamanho(Senha).
+checaTamanho(Senha) :- length(Senha, R), R > 5.
+checaCaracterEspecial([H|T]) :- member(H, [*, !, @, /, #]); checaCaracterEspecial(T). 
 
 %Le uma entrada e transforma em um string
 lerEntrada(Entrada):- read_line_to_codes(user_input, E), atom_string(E,Entrada).
@@ -73,9 +75,11 @@ lerEntrada(Entrada):- read_line_to_codes(user_input, E), atom_string(E,Entrada).
 %Le uma entrada e converte para numero
 lerNumero(Numero) :- read_line_to_codes(user_input, E), atom_string(E,R), atom_number(R,Numero).
 
+%Testando formas de instanciar o usuario, com 3(Login, Senha, Contas) parametros e so com 2(Login, Senha)
 :- dynamic(usuario/3).
 usuario(Nome, Senha, User) :- User = usuario(Nome, Senha, []).
 usuario(Nome, Senha, Conta, User) :- User = usuario(Nome, Senha, [Conta]).
 
+%Ainda não testado
 adicionaConta(Nome, Senha, Conta, NovoUser) :- usuario(Nome, Senha,Contas), append([Conta], Contas, NovasContas), NovoUser = usuario(Nome, Senha, NovasContas).
 
